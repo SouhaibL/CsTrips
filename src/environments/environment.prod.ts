2c810a089c60e1f8b5ae7f5eb7d2bc764974c392
NewVint <labbouz.souhaib@gmail.com>
2020-03-19 23:24:22 +0100

    fix deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AAAAsthvhCU:APA91bGUgGaUkaI1ooMDJJkrEmjBC9qZ9u4LiLMNtUaEQcPdTbZu3UV04QU2J5K2wJSsf2WmkczIIA-QHBQoj0nW6kDdMLMN04N-sxpOm-G-_L6g6W0Kqxhc7_BGbW0JRIfusew1UAY3',
+    authDomain: 'project-768135365669',
+    databaseURL: 'https://couchsurfing-2b006.firebaseio.com/',
+    projectId: 'couchsurfing-2b006',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '<your-messaging-sender-id>'
+  }
 };
